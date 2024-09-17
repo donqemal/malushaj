b9e1bf1e9aa4fd0201234a4b2f801f50598f82f4
Edon Malushaj <edon_malushaj@hotmail.com>
2024-09-17 21:01:09 +0200

    Many things

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,4 +11,9 @@ import {RouterLink} from "@angular/router";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  search(query: string) {
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+    window.open(searchUrl, '_blank');
+  }
 }
